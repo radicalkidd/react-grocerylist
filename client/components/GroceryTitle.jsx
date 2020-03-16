4c34388c30770fdbd2e54f6189f8b538f5988ecc
Vu Huynh <vqhuynh3@gmail.com>
2020-03-16 11:05:40 -0700

    Center Title using Flexbox

--- client/components/GroceryTitle.jsx
+++ client/components/GroceryTitle.jsx
@@ -4,6 +4,8 @@ import styled from 'styled-components';
 const Title = styled.h1 `
   font-size: 2em;
   color: salmon;
+  display: flex;
+  justify-content: center;
 `
 
 const GroceryTitle = props => (
