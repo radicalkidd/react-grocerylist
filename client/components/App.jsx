fc3171a8a3c43c5d9a0306e3c55726a5dcf960f7
Vu Huynh <vqhuynh3@gmail.com>
2020-03-21 13:00:51 -0700

    Align css and create text box

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -7,6 +7,18 @@ import GroceryTitle from './GroceryTitle.jsx';
 const Wrapper = styled.section`
   display: flex;
   justify-content: center;
+  padding-left: 15%;
+  padding-bottom: 1%;
+`
+const Box = styled.section`
+  position: fixed;
+  top: 30%;
+  left: 45%;
+  margin-top: -70px;
+  margin-left: -190px;
+  height: 200px;
+  width: 200px;
+  background-color: rgba(33, 33, 33, .3);
 `
 
 
@@ -39,12 +51,15 @@ class App extends Component {
       <div>
         <GroceryTitle />
         <Wrapper>
-        <form className="App" onSubmit={this.submitHandler}>
-          <input value={this.state.word} onChange={this.onChange} />
+        <form className="form-group field" onSubmit={this.submitHandler}>
+          <input className="form-field" placeholder="item" value={this.state.word} onChange={this.onChange} />
+          {/* <label className="form-label">Name</label> */}
           <button className="addButton">Add Item</button>
         </form>
         </Wrapper>
+        <Box>
         <CurrentList items={this.state.items}/>
+        </Box>
       </div>
     );
   }
--- client/components/CurrentList.jsx
+++ client/components/CurrentList.jsx
@@ -3,14 +3,18 @@ import styled from 'styled-components';
 
 const List = styled.section `
   display: flex;
-  justify-content: center;
+  padding-left: 5%;
 `
 
 const CurrentList = props => (
   <List>
   <ul>
     {
-      props.items.map((item, idx) => <li key={idx}>{item}</li>)
+      props.items.map((item, idx) => 
+      <li key={idx}>
+        {item[0].toUpperCase() + item.slice(1)}
+      </li>
+      )
     }
   </ul>
   </List>
